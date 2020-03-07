1500fb74cb930e0799536c7b76da27f21b284ec6
Yenying-Ho <a26782871@gmail.com>
2020-03-07 21:15:56 +0800

    Create header.php

---
+++ _require/header.php
@@ -0,0 +1,16 @@
+<meta charset="utf-8" />
+<link rel="apple-touch-icon" sizes="76x76" href="../../assets/img/apple-icon.png">
+<link rel="icon" type="image/png" href="../../images/favicon.ico">
+<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+<title>HugDog 後台管理介面</title>
+<meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+<!-- google fonts -->
+<link href="https://fonts.googleapis.com/css?family=Noto+Sans+TC:100,400&display=swap" rel="stylesheet">
+<!-- font awesome -->
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.11.2/css/all.min.css" rel="stylesheet">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
+<!-- CSS Files -->
+<link href="../../assets/css/bootstrap.min.css" rel="stylesheet" />
+<link href="../../assets/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
+<!-- 公版css -->
+<link rel="stylesheet" href="../../css/style.css">
